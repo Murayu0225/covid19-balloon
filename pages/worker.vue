5e76594c02b3fdf798a0f5afd281705f5d4150eb
Yu Muramatsu <37988559+Murayu0225@users.noreply.github.com>
2021-05-12 11:20:46 +0900

    独自に変更

--- pages/worker.vue
+++ pages/worker.vue
@@ -6,15 +6,15 @@
     <static-card>
       <h3>
         <app-link
-          to="https://covid19.supportnavi.metro.tokyo.lg.jp/"
+          to="https://www.city.sagamihara.kanagawa.jp/sangyo/sangyo/1019826/index.html"
           :icon-size="24"
-          >{{ $t('東京都 新型コロナウイルス感染症 支援情報ナビ') }}
+          >{{ $t('相模原市 新型コロナウイルス感染症 事業者支援情報') }}
         </app-link>
       </h3>
       <p>
         {{
           $t(
-            '東京都及び国の新型コロナウイルス感染症に関する支援策の情報をまとめています。'
+            '相模原市及び国の新型コロナウイルス感染症に関する支援策の情報をまとめています。'
           )
         }}
       </p>
@@ -22,31 +22,51 @@
     <static-card>
       <h3>
         <app-link
-          to="https://www.sangyo-rodo.metro.tokyo.lg.jp/topics/jitan/index.html"
+          to="https://www.city.sagamihara.kanagawa.jp/shisei/seisaku/1022660.html"
           :icon-size="24"
-          >{{ $t('東京都 感染拡大防止協力金') }}
+          >{{ $t('新型コロナウイルス感染症「暮らし・経済支援対策」') }}
         </app-link>
       </h3>
       <p>
         {{
           $t(
-            '新型コロナウイルス感染拡大防止のため、都の要請に全面的にご協力いただける事業者の皆様に対し、協力金を支給します。'
+            '本市では、新型コロナウイルス感染症の感染拡大防止対策を推進し、市民の皆様の命と暮らしを守るため、その時々の状況に合わせて必要な対策を講じています。対策の実施に当たっては、国や県の補助金等のほか、「新型コロナウイルス感染症対応地方創生臨時交付金」を活用しながら、本市の状況に合わせた市独自の対策を行っています。'
           )
         }}
       </p>
     </static-card>
     <static-card>
       <h3>
-        <app-link to="https://smooth-biz.metro.tokyo.lg.jp/" :icon-size="24"
+        <app-link
+          to="https://www.city.sagamihara.kanagawa.jp/sangyo/sangyo/1019826/1022793.html"
+          :icon-size="24"
+          >{{ $t('一時支援金【申請受付期限：令和3年5月31日（月曜日）】') }}
+        </app-link>
+      </h3>
+      <p>
+        {{
+          $t(
+            '緊急事態宣言に伴う飲食店時短営業又は外出自粛等の影響を受けており、1～3月のいずれかの月の売上が2019年比又は2020年比で50％以上減少した中小法人・個人事業者のための一時支援金'
+          )
+        }}
+      </p>
+    </static-card>
+    <static-card>
+      <h3>
+        <app-link
+          to="https://www.pref.kanagawa.jp/docs/jf2/coronavirus/kyoryokukin_8th.html"
+          :icon-size="24"
           >{{
-            $t('新しいワークスタイルや企業活動の東京モデル「スムーズビズ」')
+            $t(
+              '新型コロナウイルス感染症拡大防止協力金（第8弾）（時短営業要請期間：令和3年4月1日から4月19日まで）'
+            )
           }}
         </app-link>
       </h3>
       <p>
         {{
           $t(
-            'テレワーク・時差出勤などスムーズビズの取組は、新型コロナウイルス感染症の対策としても効果的です。感染症対策として、東京2020大会時の交通混雑緩和に向けた取組の前倒しをお願いします。'
+            '県の要請に応じて、夜間営業時間の短縮にご協力いただいた事業者に対する協力金（時短営業要請期間：令和3年4月1日から4月19日まで）'
           )
         }}
       </p>
@@ -54,15 +74,19 @@
     <static-card>
       <h3>
         <app-link
-          to="https://tokyo-telework.jp/lp/2002app/index.html"
+          to="https://www.pref.kanagawa.jp/docs/jf2/coronavirus/kyoryokukin_9th.html"
           :icon-size="24"
-          >{{ $t('TOKYOテレワークアプリ') }}
+          >{{
+            $t(
+              '新型コロナウイルス感染症拡大防止協力金（第9弾）（時短営業要請期間：令和3年4月20日から5月11日まで）'
+            )
+          }}
         </app-link>
       </h3>
       <p>
         {{
           $t(
-            'テレワークの導入・実践に必要な情報を入手できるほか、セミナー等のお申込みやサテライトオフィス等の検索など、テレワークの推進を支援する東京都公式アプリです。'
+            '県の要請に応じて、夜間営業時間の短縮にご協力いただいた事業者に対する協力金（時短営業要請期間：令和3年4月20日から5月11日まで）'
           )
         }}
       </p>
