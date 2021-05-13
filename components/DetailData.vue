52cff1bbfbbee4606de0cf1bc22cc5fff356c7a8
Yu Muramatsu <37988559+Murayu0225@users.noreply.github.com>
2021-05-14 08:10:23 +0900

    不要部分を削除

--- components/DetailData.vue
+++ components/DetailData.vue
@@ -20,14 +20,14 @@
       <div class="InfectionMedicalcareprovisionStatus-description">
         {{ $t('1日当たりの検査人数')
         }}<span
-          >{{ statuses.data['1日当たりの検査人数'].toLocaleString() }}人</span
+          >{{ statuses['1日当たりの検査人数'].toLocaleString() }}人</span
         >、{{ $t('累計検査人数')
-        }}<span>{{ statuses.data['累計検査人数'].toLocaleString() }}人</span>、
+        }}<span>{{ statuses['累計検査人数'].toLocaleString() }}人</span>、
         {{ $t('1日当たりの検査件数')
         }}<span
-          >{{ statuses.data['1日当たりの検査件数'].toLocaleString() }}件</span
+          >{{ statuses['1日当たりの検査件数'].toLocaleString() }}件</span
         >、{{ $t('累計検査件数')
-        }}<span>{{ statuses.data['累計検査件数'].toLocaleString() }}件</span>
+        }}<span>{{ statuses['累計検査件数'].toLocaleString() }}件</span>
         {{ $t('（PCR検査更新日時') }}
         {{ pcrdate }}更新）
       </div>
@@ -42,34 +42,34 @@
       </div>
       <div class="InfectionMedicalcareprovisionStatus-description">
         {{ $t('男性')
-        }}<span>{{ statuses.data['男性'].toLocaleString() }}人</span>、
+        }}<span>{{ statuses['男性'].toLocaleString() }}人</span>、
         {{ $t('女性')
-        }}<span>{{ statuses.data['女性'].toLocaleString() }}人</span>、
+        }}<span>{{ statuses['女性'].toLocaleString() }}人</span>、
         {{ $t('累計陽性者数')
-        }}<span>{{ statuses.data['累計陽性者数'].toLocaleString() }}人</span
+        }}<span>{{ statuses['累計陽性者数'].toLocaleString() }}人</span
         ><br />
         {{ $t('10歳未満：')
-        }}<span>{{ statuses.data['10歳未満'].toLocaleString() }}人</span>、
+        }}<span>{{ statuses['10歳未満'].toLocaleString() }}人</span>、
         {{ $t('10代：')
-        }}<span>{{ statuses.data['10代'].toLocaleString() }}人</span>、
+        }}<span>{{ statuses['10代'].toLocaleString() }}人</span>、
         {{ $t('20代：')
-        }}<span>{{ statuses.data['20代'].toLocaleString() }}人</span>、
+        }}<span>{{ statuses['20代'].toLocaleString() }}人</span>、
         {{ $t('30代：')
-        }}<span>{{ statuses.data['30代'].toLocaleString() }}人</span>、
+        }}<span>{{ statuses['30代'].toLocaleString() }}人</span>、
         {{ $t('40代：')
-        }}<span>{{ statuses.data['40代'].toLocaleString() }}人</span>、
+        }}<span>{{ statuses['40代'].toLocaleString() }}人</span>、
         {{ $t('50代：')
-        }}<span>{{ statuses.data['50代'].toLocaleString() }}人</span>、
+        }}<span>{{ statuses['50代'].toLocaleString() }}人</span>、
         {{ $t('60代：')
-        }}<span>{{ statuses.data['60代'].toLocaleString() }}人</span>、
+        }}<span>{{ statuses['60代'].toLocaleString() }}人</span>、
         {{ $t('70代：')
-        }}<span>{{ statuses.data['70代'].toLocaleString() }}人</span>、
+        }}<span>{{ statuses['70代'].toLocaleString() }}人</span>、
         {{ $t('80代：')
-        }}<span>{{ statuses.data['80代'].toLocaleString() }}人</span>、
+        }}<span>{{ statuses['80代'].toLocaleString() }}人</span>、
         {{ $t('90代：')
-        }}<span>{{ statuses.data['90代'].toLocaleString() }}人</span>、
+        }}<span>{{ statuses['90代'].toLocaleString() }}人</span>、
         {{ $t('100歳以上：')
-        }}<span>{{ statuses.data['100歳以上'].toLocaleString() }}人</span>
+        }}<span>{{ statuses['100歳以上'].toLocaleString() }}人</span>
       </div>
     </div>
   </div>
